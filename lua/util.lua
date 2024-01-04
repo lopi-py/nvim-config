b9640ddbf8be28e10992848787f643cbdd217555
lopy <70210066+lopi-py@users.noreply.github.com>
2024-01-04 08:09:55 -0500

    fix: silent by default in keymap patch

--- lua/util.lua
+++ lua/util.lua
@@ -57,7 +57,7 @@ function M.patch_keymap_set()
   ---@diagnostic disable-next-line: duplicate-set-field
   vim.keymap.set = function(mode, lhs, rhs, opts)
     opts = opts or {}
-    opts.silent = vim.F.if_nil()
+    opts.silent = vim.F.if_nil(opts.silent, true)
     keymap_set(mode, lhs, rhs, opts)
   end
 end
