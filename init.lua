1ea16e994b0dc1b46758a602cdd7af24c4fb89f8
lopy <70210066+lopi-py@users.noreply.github.com>
2024-01-04 08:25:39 -0500

    refactor: create a file for lazy bootstrap

--- init.lua
+++ init.lua
@@ -3,12 +3,4 @@ vim.loader.enable()
 require "config.options"
 require "config.keymaps"
 require "config.autocmds"
-
-require("config.lazy").setup {
-  { import = "plugins.extras.lua" },
-  { import = "plugins.extras.luau" },
-  { import = "plugins.extras.clang" },
-  { import = "plugins.extras.typescript" },
-  { import = "plugins.extras.json" },
-  { import = "plugins.extras.yaml" },
-}
+require "lazy-bootstrap"
---
+++ lua/lazy-bootstrap.lua
@@ -0,0 +1,54 @@
+local icons = require "config.icons"
+local util = require "util"
+
+local lazy_root = util.path_join(vim.fn.stdpath "data", "lazy")
+local lazy_path = util.path_join(lazy_root, "lazy.nvim")
+local dev_path = util.path_join(util.on_windows and "F:" or "~", "dev", "nvim")
+
+if not util.path_exists(lazy_path) then
+  vim.fn.system {
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    lazy_path,
+  }
+end
+
+vim.opt.rtp:append(lazy_path)
+
+require("lazy").setup({
+  { import = "plugins" },
+  { import = "plugins.extras" },
+}, {
+  root = lazy_root,
+  defaults = {
+    lazy = true,
+  },
+  dev = {
+    path = dev_path,
+    patterns = { "lopi-py" },
+  },
+  ui = {
+    border = icons.border,
+  },
+  change_detection = {
+    enabled = false,
+  },
+  performance = {
+    rtp = {
+      disabled_plugins = {
+        "gzip",
+        "matchit",
+        "matchparen",
+        "netrwPlugin",
+        "rplugin",
+        "spellfile",
+        "tarPlugin",
+        "tohtml",
+        "tutor",
+        "zipPlugin",
+      },
+    },
+  },
+})
