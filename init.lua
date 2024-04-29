b4dbbffbc8479ffcc97d8d46351fc374e37da669
email <70210066+lopi-py@users.noreply.github.com>
2024-04-28 19:33:53 -0500

    feat: use vim.system instead of vim.fn.system

--- init.lua
+++ init.lua
@@ -4,13 +4,15 @@ require "config.autocmds"
 
 local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
 if not vim.uv.fs_stat(lazypath) then
-  vim.fn.system {
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    lazypath,
-  }
+  vim
+    .system({
+      "git",
+      "clone",
+      "--filter=blob:none",
+      "https://github.com/folke/lazy.nvim.git",
+      lazypath,
+    })
+    :wait()
 end
 vim.opt.rtp:append(lazypath)
 
