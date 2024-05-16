7bd305cd0c2ddef331838f8b10d260057c7dd170
email <70210066+lopi-py@users.noreply.github.com>
2024-05-16 11:15:40 -0500

    refactor: remove semshi

--- lua/plugins/lang/python.lua
+++ lua/plugins/lang/python.lua
@@ -1,36 +1,5 @@
 return {
   {
-    "wookayin/semshi",
-    event = { "LazyFile" },
-    build = ":UpdateRemotePlugins",
-    init = function()
-      vim.g["semshi#error_sign"] = false
-      vim.g["semshi#simplify_markup"] = false
-      vim.g["semshi#mark_selected_nodes"] = false
-      vim.g["semshi#update_delay_factor"] = 0.001
-      vim.g["semshi#excluded_hl_groups"] = { "unresolved" }
-
-      local function update_highlights()
-        vim.api.nvim_set_hl(0, "semshiGlobal", { italic = true })
-        vim.api.nvim_set_hl(0, "semshiImported", { link = "@lsp.type.namespace" })
-        vim.api.nvim_set_hl(0, "semshiLocal", { link = "@variable" })
-        vim.api.nvim_set_hl(0, "semshiParameter", { link = "@lsp.type.parameter" })
-        vim.api.nvim_set_hl(0, "semshiParameterUnused", { link = "DiagnosticUnnecessary" })
-        vim.api.nvim_set_hl(0, "semshiBuiltin", { link = "@function.builtin" })
-        vim.api.nvim_set_hl(0, "semshiAttribute", { link = "@field" })
-        vim.api.nvim_set_hl(0, "semshiSelf", { link = "@lsp.type.selfKeyword" })
-        vim.api.nvim_set_hl(0, "semshiFree", { link = "@comment" })
-      end
-
-      vim.api.nvim_create_autocmd("ColorScheme", {
-        callback = update_highlights,
-      })
-
-      update_highlights()
-    end,
-  },
-
-  {
     "linux-cultist/venv-selector.nvim",
     cmd = { "VenvSelect" },
     opts = {
