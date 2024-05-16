00013923a011b149aac13b339cd79ecdb162d7d0
email <70210066+lopi-py@users.noreply.github.com>
2024-05-16 11:17:08 -0500

    refactor: remove some options i didn't use

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,25 +1,6 @@
 vim.api.nvim_create_autocmd("TextYankPost", {
   callback = function()
-    vim.highlight.on_yank { hlgroup = "Visual", timeout = 200 }
-  end,
-})
-
-vim.api.nvim_create_autocmd({ "InsertLeave", "WinEnter" }, {
-  callback = function()
-    local ok, value = pcall(vim.api.nvim_win_get_var, 0, "auto-cursorline")
-    if ok and value then
-      vim.wo.cursorline = true
-      vim.api.nvim_win_del_var(0, "auto-cursorline")
-    end
-  end,
-})
-
-vim.api.nvim_create_autocmd({ "InsertEnter", "WinLeave" }, {
-  callback = function()
-    if vim.wo.cursorline then
-      vim.api.nvim_win_set_var(0, "auto-cursorline", true)
-      vim.wo.cursorline = false
-    end
+    vim.highlight.on_yank()
   end,
 })
 
--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,34 +2,18 @@ require("util").patch_keymap_set()
 
 local map = vim.keymap.set
 
-map("n", "[b", vim.cmd.bprevious)
-map("n", "]b", vim.cmd.bnext)
-map("n", "[t", vim.cmd.tabprevious)
-map("n", "]t", vim.cmd.tabnext)
-
-map("n", "<leader>tn", vim.cmd.tabnew)
-map("n", "<leader>tc", vim.cmd.tabclose)
 map({ "i", "n" }, "<esc>", "<cmd>noh<cr><esc>")
-map("n", "<leader>w", vim.cmd.write, { desc = "Save" })
-map("n", "<leader>W", vim.cmd.wall, { desc = "Save all" })
-map("n", "<leader>q", vim.cmd.quit, { desc = "Quit" })
-map("n", "<leader>x", vim.cmd.quitall, { desc = "Quit all" })
-map("n", "<c-n>", vim.cmd.enew)
+
+map("n", "<leader>w", vim.cmd.write)
+
+map("n", "<tab>", vim.cmd.tabnext)
+map("n", "<s-tab>", vim.cmd.tabprevious)
+
+map("n", "<leader>tx", vim.cmd.tabclose)
 map("n", "<leader>bp", vim.cmd.bprevious)
 map("n", "<leader>bn", vim.cmd.bnext)
 map("n", "<leader>br", vim.cmd.edit)
-map("n", "<bs>", function()
-  vim.cmd.edit "#"
-end)
-
-map("n", "<a-j>", "<cmd>m .+1<cr>==")
-map("n", "<a-k>", "<cmd>m .-2<cr>==")
-map("i", "<a-j>", "<esc><cmd>m .+1<cr>==gi")
-map("i", "<a-k>", "<esc><cmd>m .-2<cr>==gi")
-map("v", "<a-j>", ":m '>+1<cr>gv=gv")
-map("v", "<a-k>", ":m '<-2<cr>gv=gv")
-
-map("v", "<", "<gv")
-map("v", ">", ">gv")
+map("n", "<leader>bx", vim.cmd.bdelete)
+map("n", "<bs>", ":e #<cr>")
 
 map("t", "<c-w>", "<c-\\><c-n><c-w>", { remap = true })
