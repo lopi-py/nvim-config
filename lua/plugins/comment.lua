502233d2cbe91a3346b19b68c47e73bbdacd2dda
email <70210066+lopi-py@users.noreply.github.com>
2024-05-21 22:30:51 -0500

    feat: use folke/ts-comments.nvim instead of nvim-ts-context-commentstring

--- lua/plugins/comment.lua
+++ lua/plugins/comment.lua
@@ -4,22 +4,13 @@ return {
     event = { "VeryLazy" },
     opts = {
       options = {
-        custom_commentstring = function()
-          return require("ts_context_commentstring").calculate_commentstring()
-            or vim.bo.commentstring
-        end,
         ignore_blank_line = true,
       },
     },
     dependencies = {
       {
-        "JoosepAlviste/nvim-ts-context-commentstring",
-        opts = {
-          enable_autocmd = false,
-        },
-        init = function()
-          vim.g.skip_ts_context_commentstring_module = true
-        end,
+        "folke/ts-comments.nvim",
+        opts = {},
       },
     },
   },
