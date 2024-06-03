c9db46888a7931154bc4af64616ae6384c9db260
email <70210066+lopi-py@users.noreply.github.com>
2024-06-02 20:24:51 -0500

    fix: update trouble v3 setup

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -2,11 +2,9 @@ return {
   {
     "folke/trouble.nvim",
     keys = {
-      { "<leader>tt", ":TroubleToggle<cr>", desc = "Troggle trouble" },
+      { "<leader>xx", ":Trouble diagnostics toggle<cr>", desc = "Diagnostics (Trouble)" },
     },
-    opts = {
-      use_diagnostic_signs = true,
-    },
+    opts = {},
     dependencies = {
       "nvim-tree/nvim-web-devicons",
     },
