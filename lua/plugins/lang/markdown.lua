b4d2057ce86dacf6cdc9e24dc11518fb94efb678
lopy <70210066+lopi-py@users.noreply.github.com>
2024-06-26 23:11:47 -0500

    feat: add markdown visualizer

---
+++ lua/plugins/lang/markdown.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    "MeanderingProgrammer/markdown.nvim",
+    ft = { "markdown" },
+    main = "render-markdown",
+    opts = {
+      code_style = "normal",
+    },
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter",
+      "nvim-tree/nvim-web-devicons",
+    },
+  },
+}
