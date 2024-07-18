275a9c43eb72fd725a9a860a8d88cad023e2734a
lopy <70210066+lopi-py@users.noreply.github.com>
2024-07-18 15:39:33 -0500

    feat: set luau config if a rojo file is found

--- lua/plugins/lang/luau.lua
+++ lua/plugins/lang/luau.lua
@@ -1,19 +1,27 @@
+local function find_rojo_file()
+  local cwd = assert(vim.uv.cwd())
+  return vim.fs.root(cwd, function(path)
+    return path:match "%.project.json$"
+  end)
+end
+
 return {
   {
     "lopi-py/luau-lsp.nvim",
     event = { "LazyFile" },
     opts = function()
-      local capabilities = require("lsp").capabilities
+      local rojo_file = find_rojo_file()
 
       return {
         server = {
-          capabilities = capabilities(),
+          capabilities = require("lsp").capabilities(),
           settings = {
             ["luau-lsp"] = {
               ignoreGlobs = { "**/_Index/**", "**/node_modules/**" },
               completion = {
                 imports = {
                   enabled = true,
+                  ignoreGlobs = { "**/_Index/**", "**/node_modules/**" },
                 },
               },
               inlayHints = {
@@ -22,6 +30,12 @@ return {
               },
             },
           },
+        },
+        platform = {
+          type = rojo_file and "roblox" or "standard",
+        },
+        sourcemap = {
+          enabled = rojo_file ~= nil,
         },
       }
     end,
