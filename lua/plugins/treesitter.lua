36955f01a6a8848e1cbb086eebea95e4a5ffb417
lopy <70210066+lopi-py@users.noreply.github.com>
2024-07-26 20:54:29 -0500

    fix: load treesitter properly

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -31,16 +31,16 @@ return {
   init = function()
     -- schedule highlight avoid delays when opening a typescript file because the parser is slow to startup
     vim.api.nvim_create_autocmd("FileType", {
-      callback = vim.schedule_wrap(function()
-        local has_parser = pcall(vim.treesitter.get_parser)
+      callback = vim.schedule_wrap(function(event)
+        local has_parser = pcall(vim.treesitter.get_parser, event.buf)
         if not has_parser then
           return
         end
 
-        vim.treesitter.start()
+        vim.treesitter.start(event.buf)
         vim.wo.foldmethod = "expr"
         vim.wo.foldexpr = "v:lua.vim.treesitter.foldexpr()"
-        vim.bo.indentexpr = "v:lua.require('nvim-treesitter').indentexpr()"
+        vim.b[event.buf].indentexpr = "v:lua.require('nvim-treesitter').indentexpr()"
       end),
     })
   end,
