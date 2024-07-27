657896792ce57e9f0e844a498215bbbe645da086
lopy <70210066+lopi-py@users.noreply.github.com>
2024-07-27 00:05:54 -0500

    remove unused utils

--- lua/util.lua
+++ lua/util.lua
@@ -1,7 +1,4 @@
-local config = require "config"
-
 local M = {}
-M.on_windows = vim.loop.os_uname().sysname == "Windows_NT"
 
 ---@param bufnr number
 ---@return boolean
@@ -16,28 +13,6 @@ function M.is_file(bufnr)
   end
 
   return true
-end
-
----@param bufnr number
----@return boolean
-function M.within_fsize(bufnr)
-  local success, stats = pcall(vim.uv.fs_stat, vim.api.nvim_buf_get_name(bufnr))
-  if success and stats and stats.size > config.max_file_size then
-    return false
-  end
-  return true
-end
-
----@vararg string
----@return string
-function M.path_join(...)
-  return table.concat({ ... }, M.on_windows and "\\" or "/")
-end
-
----@param path string
----@return boolean
-function M.path_exists(path)
-  return vim.loop.fs_stat(path) ~= nil
 end
 
 function M.patch_keymap_set()
