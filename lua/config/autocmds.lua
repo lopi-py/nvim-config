3945e8a45f745a6758c6f3b4d5e07d619df00750
lopy <70210066+lopi-py@users.noreply.github.com>
2024-07-27 00:12:34 -0500

    remove unused param

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -2,7 +2,7 @@ local autocmd = vim.api.nvim_create_autocmd
 
 autocmd({ "BufReadPost", "BufNewFile" }, {
   once = true,
-  callback = function(event)
+  callback = function()
     vim.schedule(function()
       vim.api.nvim_exec_autocmds("User", { pattern = "FilePost", modeline = false })
       vim.api.nvim_exec_autocmds("FileType", {})
