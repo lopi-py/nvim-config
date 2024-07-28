4d00820aad182d6517bffe60620c0647e8bfc77f
lopy <70210066+lopi-py@users.noreply.github.com>
2024-07-28 18:00:52 -0500

    remove unused util

--- lua/util.lua
+++ lua/util.lua
@@ -1,20 +1,5 @@
 local M = {}
 
----@param bufnr number
----@return boolean
-function M.is_file(bufnr)
-  if not vim.api.nvim_buf_is_valid(bufnr) then
-    return false
-  end
-
-  local path = vim.api.nvim_buf_get_name(bufnr)
-  if path == "" or vim.bo[bufnr].buftype ~= "" then
-    return false
-  end
-
-  return true
-end
-
 function M.patch_keymap_set()
   local keymap_set = vim.keymap.set
   ---@diagnostic disable-next-line: duplicate-set-field
