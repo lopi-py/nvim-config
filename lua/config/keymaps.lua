fc477e3abe8650ce755ca1ffab6e80b9edd6532e
lopy <70210066+lopi-py@users.noreply.github.com>
2024-07-28 21:54:08 -0500

    change some keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,13 +2,14 @@ require("util").patch_keymap_set()
 
 local map = vim.keymap.set
 
-map({ "i", "n" }, "<esc>", "<cmd>nohlsearch<cr><esc>")
+map("n", "<esc>", ":nch<cr><esc>")
 map("n", "<leader>w", vim.cmd.write)
-map("n", "<bs>", ":e #<cr>")
 map("n", "<tab>", vim.cmd.tabnext)
 map("n", "<s-tab>", vim.cmd.tabprevious)
 map("n", "<leader>tx", vim.cmd.tabclose)
 map("n", "<leader>bx", vim.cmd.bdelete)
+map("n", "<bs>", ":e #<cr>")
+
 map("t", "<c-w>", "<c-\\><c-n><c-w>", { remap = true })
 
 map("n", "<leader>uh", function()
@@ -29,6 +30,7 @@ map({ "i", "s" }, "<c-j>", function()
     vim.snippet.jump(1)
   end
 end)
+
 map({ "i", "s" }, "<c-k>", function()
   if vim.snippet.active { direction = -1 } then
     vim.snippet.jump(-1)
