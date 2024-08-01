ab373383cf93631f6b58b523b4c87e96e5b6b892
lopy <70210066+lopi-py@users.noreply.github.com>
2024-08-01 16:21:12 -0500

    use vim.schedule_wrap in autocmds

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -2,12 +2,10 @@ local autocmd = vim.api.nvim_create_autocmd
 
 autocmd({ "BufReadPost", "BufNewFile" }, {
   once = true,
-  callback = function()
-    vim.schedule(function()
-      vim.api.nvim_exec_autocmds("User", { pattern = "FilePost", modeline = false })
-      vim.api.nvim_exec_autocmds("FileType", {})
-    end)
-  end,
+  callback = vim.schedule_wrap(function()
+    vim.api.nvim_exec_autocmds("User", { pattern = "FilePost", modeline = false })
+    vim.api.nvim_exec_autocmds("FileType", {})
+  end),
 })
 
 autocmd("TextYankPost", {
