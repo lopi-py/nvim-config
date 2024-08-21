d6354194d932429d4634357b528826185c6d8322
lopy <70210066+lopi-py@users.noreply.github.com>
2024-08-20 23:45:23 -0500

    remove unused

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -40,12 +40,6 @@ return {
     "hrsh7th/cmp-buffer",
     {
       "garymjr/nvim-snippets",
-      opts = {
-        friendly_snippets = true,
-      },
-      dependencies = {
-        "rafamadriz/friendly-snippets",
-      },
     },
   },
 }
--- lua/plugins/endwise.lua
+++
@@ -1,7 +0,0 @@
--- NOTE: https://github.com/RRethy/nvim-treesitter-endwise/pull/39
-return {
-  "RRethy/nvim-treesitter-endwise",
-  enabled = false,
-  event = "InsertEnter",
-  opts = {},
-}
