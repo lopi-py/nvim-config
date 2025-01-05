fd77c3bc663daae330e2a181693ed2674537ac73
lopy <70210066+lopi-py@users.noreply.github.com>
2025-01-04 19:38:23 -0500

    cleanup lsp

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -15,31 +15,6 @@ function M.capabilities()
   )
 end
 
-local function on_attach(_, bufnr)
-  local function map(mode, lhs, rhs)
-    vim.keymap.set(mode, lhs, rhs, { buffer = bufnr })
-  end
-
-  map("n", "gd", "<cmd>FzfLua lsp_definitions jump_to_single_result=true<cr>")
-  map("n", "gD", vim.lsp.buf.declaration)
-  map("n", "gi", "<cmd>FzfLua lsp_implementations jump_to_single_result=true<cr>")
-  map("n", "gy", "<cmd>FzfLua lsp_typedefs jump_to_single_result=true<cr>")
-  map("n", "grr", "<cmd>FzfLua lsp_references jump_to_single_result=true<cr>")
-  map("n", "grn", vim.lsp.buf.rename)
-  map({ "n", "v" }, "gra", vim.lsp.buf.code_action)
-  map("i", "<c-s>", vim.lsp.buf.signature_help)
-end
-
-vim.lsp.config("*", {
-  capabilities = {
-    workspace = {
-      didChangeWatchedFiles = {
-        dynamicRegistration = true,
-      },
-    },
-  },
-})
-
 vim.diagnostic.config {
   signs = false,
   float = {
@@ -47,26 +22,18 @@ vim.diagnostic.config {
   },
 }
 
-local register_capability = vim.lsp.handlers["client/registerCapability"]
-vim.lsp.handlers["client/registerCapability"] = function(err, result, ctx)
-  local client = vim.lsp.get_client_by_id(ctx.client_id)
-  if not client then
-    return
-  end
-
-  on_attach(client, vim.api.nvim_get_current_buf())
-
-  return register_capability(err, result, ctx)
-end
-
 vim.api.nvim_create_autocmd("LspAttach", {
   callback = function(event)
-    local client = vim.lsp.get_client_by_id(event.data.client_id)
-    if not client then
-      return
+    local function map(mode, lhs, rhs)
+      vim.keymap.set(mode, lhs, rhs, { buffer = event.buf })
     end
 
-    on_attach(client, event.buf)
+    map("n", "gd", "<cmd>FzfLua lsp_definitions jump_to_single_result=true<cr>")
+    map("n", "gD", "<cmd>FzfLua lsp_declarations jump_to_single_result=true<cr>")
+    map("n", "gy", "<cmd>FzfLua lsp_typedefs jump_to_single_result=true<cr>")
+    map("n", "grr", "<cmd>FzfLua lsp_references jump_to_single_result=true<cr>")
+    map("n", "gri", "<cmd>FzfLua lsp_implementations jump_to_single_result=true<cr>")
+    map("n", "gO", "<cmd>FzfLua lsp_document_symbols<cr>")
   end,
 })
 
